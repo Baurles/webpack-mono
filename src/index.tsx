4e7c2041098b9735fdf7384d903b33fd3a674e51
Artem <artemhmaruk@gmail.com>
2024-02-18 19:56:22 +0300

    clear monorepo

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import {createRoot} from "react-dom/client";
 import {App} from "./components/App";
 import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Shop } from "@/pages/shop";
-import { LazyAbout } from "@/pages/about/About.lazy";
-import { Suspense } from "react";
+
+
 
 const root = document.getElementById('root');
 
@@ -15,21 +13,5 @@ if(!root){
 const container = createRoot(root);
 
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App/>,
-      children: [
-        {
-            path:'/about',
-            element:<Suspense fallback={'Loading...'}><LazyAbout/></Suspense>
-        },
-        {
-            path: '/shop',
-            element:<Suspense fallback={'Loading...'}><Shop/></Suspense>
-        }
-      ]
-    },
-  ]);
 
-container.render(<RouterProvider router={router}/>)
+container.render(<App/>)
